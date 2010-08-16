ada12098a63c1c570459da49301fb70fd8c12626
bluecopter <bluecopter@9d917ac4-0eb2-cfcc-ffbc-adfc0d90c138>
2010-08-16 14:03:36 +0000

    The Colbert Report - added fanart & icon - several fixes to bring colbert up to daily plugin

--- plugin.video.the.colbert.report/default.py
+++ plugin.video.the.colbert.report/default.py
@@ -1,14 +1,17 @@
 import urllib,urllib2,re,xbmcplugin,xbmcgui
 import os
 
+pluginhandle = int(sys.argv[1])
 shownail = xbmc.translatePath(os.path.join(os.getcwd().replace(';', ''),"icon.png"))
-pluginhandle = int (sys.argv[1])
+fanart = xbmc.translatePath(os.path.join(os.getcwd().replace(';', ''),'fanart.jpg'))
+xbmcplugin.setPluginFanart(pluginhandle, fanart, color2='0xFFFF3300')
+TVShowTitle = 'The Colbert Report' 
 
-if xbmcplugin.getSetting(pluginhandle,"sort") == '0':#Relevance
+if xbmcplugin.getSetting(pluginhandle,"sort") == '0':
         SORTORDER = 'date'
-elif xbmcplugin.getSetting(pluginhandle,"sort") == '1':#Date Ascending
+elif xbmcplugin.getSetting(pluginhandle,"sort") == '1':
         SORTORDER = 'views'
-elif xbmcplugin.getSetting(pluginhandle,"sort") == '2':#Date Descending
+elif xbmcplugin.getSetting(pluginhandle,"sort") == '2':
         SORTORDER = 'rating'
 
 ################################ Common
@@ -32,41 +35,57 @@ def getURL( url ):
 def addLink(name,url,iconimage='',plot=''):
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot":plot})
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot":plot, "TVShowTitle":TVShowTitle})
+        liz.setProperty('fanart_image',fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=pluginhandle,url=url,listitem=liz)
         return ok
 
 def addDir(name,url,mode,iconimage=shownail,plot=''):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot":plot})
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot":plot, "TVShowTitle":TVShowTitle})
+        liz.setProperty('fanart_image',fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=pluginhandle,url=u,listitem=liz,isFolder=True)
         return ok
 
 def pageFragments(url):
         data = getURL(url)
         try:
-                nextpage=re.compile('href="(.+?)" class="nextprev" title="Go to Next Page"').findall(data)[0]
+                nextpage=re.compile('><span class="pageOff"><a href="(.+?)" class="nextprev" title="Go to Next Page">').findall(data)[0]
                 nextpage = 'http://www.colbertnation.com'+nextpage
                 addDir('Next Page',nextpage,7)
         except:
                 print 'Last or Single page'
-        LISTVIDEO(url) 
+        try:
+                prevpage=re.compile('\t<span class="pageOff"><a href="(.+?)" class="nextprev" title="Go to Previous Page">').findall(data)[0]
+                prevpage = 'http://www.colbertnation.com'+prevpage
+                addDir('Previous Page',prevpage,7)
+        except:
+                print 'No Previous page'
+        LISTVIDEOS(url)
+        xbmcplugin.endOfDirectory(pluginhandle,updateListing=True)
 
 ################################ Root listing
 def ROOT():
         addDir('Full Episodes','full',5)
         addDir('Segments','segments',4)
         addDir('Guests','guests',3)
+        xbmcplugin.endOfDirectory(pluginhandle)
 
 def FULLEPISODES():
-        for week in range(0,4):
-            url = 'http://www.colbertnation.com/feeds/full-episode/showcase/258210/'+str(week)+'/343150'
+        xbmcplugin.setContent(pluginhandle, 'episodes')
+        xbmcplugin.addSortMethod(pluginhandle, xbmcplugin.SORT_METHOD_NONE)
+        full = 'http://www.colbertnation.com/full-episodes/'
+        data = getURL(full)
+        weeks = re.compile('<a id="(.+?)" class="seaso.+?" href="#">(.+?)</a>').findall(data)
+        for url, week in weeks:
             data = getURL(url)
             episodes=re.compile('<span class="date"><a href="(.+?)">(.+?)</a></span>').findall(data)
             thumbnails=re.compile('<img width="156" height="86" src="(.+?)\?width=156" border="0"/>').findall(data)
             descriptions=re.compile('<span class="description">(.+?)</span>').findall(data)
+            airdates=re.compile('<span class="date">Aired: (.+?)</span>').findall(data)
+            epNumbers=re.compile('<span class="id">Episode (.+?)</span>').findall(data)
             listings = []
             for link, name in episodes:
                 listing = []
@@ -75,12 +94,32 @@ def FULLEPISODES():
                 listings.append(listing)
             for thumbnail in thumbnails:
                 marker = thumbnails.index(thumbnail)
-                listings[marker].append(thumbnail)
+                listings[marker].append(thumbnail+'?width=400')
             for description in descriptions:
                 marker = descriptions.index(description)
                 listings[marker].append(description)
-            for name, link, thumbnail, plot in listings:
-                addDir(name,link,10,thumbnail,plot)
+            for airdate in airdates:
+                marker = airdates.index(airdate)
+                listings[marker].append(airdate)
+            for epNumber in epNumbers:
+                marker = epNumbers.index(epNumber)
+                listings[marker].append(epNumber)
+            for name, link, thumbnail, plot, date, seasonepisode in listings:
+                mode = 10
+                season = int(seasonepisode[:-3])
+                episode = int(seasonepisode[-3:])
+                u=sys.argv[0]+"?url="+urllib.quote_plus(link)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+                liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=thumbnail)
+                liz.setInfo( type="Video", infoLabels={ "Title": name,
+                                                        "Plot":plot,
+                                                        "Season":season,
+                                                        "Episode": episode,
+                                                        "premiered":date,
+                                                        "TVShowTitle":TVShowTitle})
+                liz.setProperty('IsPlayable', 'true')
+                liz.setProperty('fanart_image',fanart)
+                xbmcplugin.addDirectoryItem(handle=pluginhandle,url=u,listitem=liz)
+        xbmcplugin.endOfDirectory(pluginhandle)
 
  
 def GUESTS():
@@ -93,6 +132,7 @@ def GUESTS():
         for name,link in segments:
             furl = segurl+link
             addDir(name,furl,7)
+        xbmcplugin.endOfDirectory(pluginhandle)
 
 def SEGMENTS():
         exclude = ('Author','Expert','Journalist','Political Figure')
@@ -104,61 +144,105 @@ def SEGMENTS():
                         continue
                 url = 'http://www.colbertnation.com'+url
                 addDir(name+count,url,7)
+        xbmcplugin.endOfDirectory(pluginhandle)
 
 
 ################################ List Videos
 
-def LISTVIDEO(url):
+def LISTVIDEOS(url):
+        xbmcplugin.setContent(pluginhandle, 'episodes')
         data = getURL(url)
-        playbackUrls=re.compile('href="http://www.colbertnation.com/the-colbert-report-videos/(.+?)"').findall(data)
-        items = []
-        for item in playbackUrls:
-                if item not in items:
-                        items.append(item)
-        for pb in items:
+        playbackUrls=re.compile('<div class="clipTitle"><a onclick="siteSearchReport\(\);"  href="http://www.colbertnation.com/the-colbert-report-videos/(.+?)"').findall(data)
+        #playbackUrls=re.compile('href="http://www.colbertnation.com/the-colbert-report-videos/(.+?)"').findall(data)
+        thumbnails=re.compile('<img src="(.+?)?width=.+?"  width=".+?"  height="71"').findall(data)
+        names=re.compile('<div class="clipTitle"><a onclick="siteSearchReport\(\);"  href="http://www.colbertnation.com/the-colbert-report-videos/.+?">(.+?)</a></div>').findall(data)
+        descriptions=re.compile('<div class="clipDescription">(.+?)\(.+?\)</div>').findall(data)
+        durations=re.compile('<div class="clipDescription">.+?\((.+?)\)</div>').findall(data)
+        epNumbers=re.compile('<span>Episode:</span> #(.+?)</div>').findall(data)
+        airdates=re.compile('<div class="clipDate">Aired: (.+?)</div>').findall(data)
+        for pb in playbackUrls:
                 url = "http://www.colbertnation.com/the-colbert-report-videos/"+pb
-                data = getURL(url)
-                fname = re.compile('<span property="media:title" content="(.+?)">').findall(data)[0]
+                marker = playbackUrls.index(pb)
+                thumbnail = thumbnails[marker]+'?width=400'
+                fname = names[marker]
+                description = descriptions[marker]
+                duration = durations[marker]
                 try:
-                    description = re.compile('<span property="dc:description" content="(.+?)">').findall(data)[0]
+                        seasonepisode = epNumbers[marker]
+                        season = int(seasonepisode[:-3])
+                        episode = int(seasonepisode[-3:])
                 except:
-                    description = ''
-                thumbnail = re.compile('<a rel="media:thumbnail" href="(.+?)">').findall(data)[0]
-                uri = re.compile('"http://media.mtvnservices.com/(.+?)"/>').findall(data)[0]
-                addDir(fname,uri,11,thumbnail,description)
-                #furl = GRAB_RTMP(uri)
-                #if furl is not False:
-                #        addLink(fname,furl,thumbnail,description)
-                        
+                        season = 0
+                        episode = 0
+                date = airdates[marker]
+                u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(11)+"&name="+urllib.quote_plus(fname)
+                liz=xbmcgui.ListItem(fname, iconImage="DefaultVideo.png", thumbnailImage=thumbnail)
+                liz.setInfo( type="Video", infoLabels={ "Title": fname,
+                                                        "Episode": episode,
+                                                        "Season": season,
+                                                        "Plot":description,
+                                                        "premiered":date,
+                                                        "Duration": duration,
+                                                        "TVShowTitle":TVShowTitle})
+                liz.setProperty('IsPlayable', 'true')
+                liz.setProperty('fanart_image',fanart)
+                xbmcplugin.addDirectoryItem(handle=pluginhandle,url=u,listitem=liz)
 
+                               
 ################################ Play Video
                 
-def PLAYVIDEO(name,uri):
-        playlist = xbmc.PlayList(1)
-        playlist.clear() 
+def PLAYVIDEO(name,url):
+        data = getURL(url)
+        #try:
+        #        fname = re.compile('property="media:title" content="(.+?)">').findall(data)[0]
+        #except:
+        #        fname = re.compile('<meta name="title" content="(.+?)"').findall(data)[0]
+        try:
+            description = re.compile('<span property="dc:description" content="(.+?)">').findall(data)[0]
+        except:
+            description = ''
+        try:
+            thumbnail = re.compile('<a rel="media:thumbnail" href="(.+?)">').findall(data)[0] + '?width=400'
+        except:
+            thumbnail = ''
+        try:
+            date = re.compile('<span property="dc:date" content="(.+?)"></span>').findall(data)[0]
+        except:
+            date = ''
+        uri = re.compile('"http://media.mtvnservices.com/(.+?)"/>').findall(data)[0]
         rtmp = GRAB_RTMP(uri)
-        item = xbmcgui.ListItem(name)
-        playlist.add(rtmp, item)
-        xbmc.Player().play(playlist)
-        xbmc.executebuiltin('XBMC.ActivateWindow(fullscreenvideo)')
+        item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumbnail, path=rtmp)
+        item.setInfo( type="Video", infoLabels={ "Title": name,
+                                                 "Plot":description,
+                                                 "premiered":date,
+                                                 "Season":0,
+                                                 "Episode":0,
+                                                 "TVShowTitle":TVShowTitle})
+        item.setProperty('fanart_image',fanart)
+        xbmcplugin.setResolvedUrl(pluginhandle, True, item)
 
 
 ################################ Play Full Episode
-                
-def PLAYEPISODE(name,url):
+        
+def PLAYFULLEPISODE(name,url):
         data = getURL(url)
         uri=re.compile('<param name="movie" value="http://media.mtvnservices.com/(.+?)"').findall(data)[0]
-        url = 'http://media.mtvnservices.com/player/config.jhtml?uri='+uri+'&group=entertainment&type=network&site=thedailyshow.com'
+        url = 'http://media.mtvnservices.com/player/config.jhtml?uri='+uri+'&group=entertainment&type=network&site=colbertnation.com'
         data = getURL(url)
+        thumbnail = 'http://www.comedycentral.com/images/shows/'+re.compile('/images/shows/(.+?)\n').findall(data)[0]
         uris=re.compile('<guid isPermaLink="false">(.+?)</guid>').findall(data)
-        playlist = xbmc.PlayList(1)
-        playlist.clear() 
+        stacked_url = 'stack://'
         for uri in uris:
             rtmp = GRAB_RTMP(uri)
-            item = xbmcgui.ListItem(name)
-            playlist.add(rtmp, item)
-        xbmc.Player().play(playlist)
-        xbmc.executebuiltin('XBMC.ActivateWindow(fullscreenvideo)')
+            stacked_url += rtmp.replace(',',',,')+' , '
+        item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=thumbnail, path=stacked_url)
+        item.setInfo( type="Video", infoLabels={ "Title": name,
+                                                 "Season":0,
+                                                 "Episode":0,
+                                                 "TVShowTitle":TVShowTitle})
+        item.setProperty('fanart_image',fanart)
+        print stacked_url
+        xbmcplugin.setResolvedUrl(pluginhandle, True, item)
         
 ################################ Grab rtmp        
 
@@ -259,23 +343,18 @@ print "Name: "+str(name)
 
 if mode==None or url==None or len(url)<1:
         ROOT()
-        xbmcplugin.endOfDirectory(int(sys.argv[1]),updateListing=False,cacheToDisc=True)
 elif mode==3:
         GUESTS()
-        xbmcplugin.endOfDirectory(int(sys.argv[1]),updateListing=False,cacheToDisc=True)
 elif mode==4:
         SEGMENTS()
-        xbmcplugin.endOfDirectory(int(sys.argv[1]),updateListing=False,cacheToDisc=True)
 elif mode==5:
         FULLEPISODES()
-        xbmcplugin.endOfDirectory(int(sys.argv[1]),updateListing=False,cacheToDisc=True)
 elif mode==7:
         pageFragments(url)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]),updateListing=False,cacheToDisc=True)
 elif mode==9:
-        LISTVIDEO(url)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]),updateListing=False,cacheToDisc=True)
+        LISTVIDEOS(url)
+        xbmcplugin.endOfDirectory(pluginhandle) 
 elif mode==10:
-        PLAYEPISODE(name,url)
+        PLAYFULLEPISODE(name,url)
 elif mode==11:
         PLAYVIDEO(name,url)

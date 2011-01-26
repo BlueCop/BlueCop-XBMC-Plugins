cc495ed002da34f7b144150023e60ac2d2135bbd
bluecopter <bluecopter@9d917ac4-0eb2-cfcc-ffbc-adfc0d90c138>
2011-01-26 21:05:39 +0000

    VEVO - added various categories      - fixed A-Z listings

--- plugin.video.vevo/default.py
+++ plugin.video.vevo/default.py
@@ -9,18 +9,102 @@ pluginhandle = int(sys.argv[1])
 BASE_URL = 'http://www.vevo.com'
 
 def listCategories():
-        addDir('A-Z', 'http://www.vevo.com/artists/index/a-z?alpha=', 1)
-        addDir('On Tour', 'http://www.vevo.com/artists/on-tour', 2)
-        addDir('Most Recent', 'http://www.vevo.com/artists?order=MostRecent', 2)
-        addDir('Most Viewed Artists', 'http://www.vevo.com/artists?order=MostViewedToday', 2)
-        addDir('Most Liked Artists', 'http://www.vevo.com/artists?order=MostFavoritedToday', 2)
+        addDir('Artists (A-Z)', 'http://www.vevo.com/artists/index/a-z?alpha=', 1)
+        addDir('Artists by Genre', 'artist', 7)
+        addDir('Artists On Tour', 'http://www.vevo.com/artists/on-tour', 2)
+        addDir('Most Recent Artists', 'http://www.vevo.com/artists?order=MostRecent', 2)
+        addDir('Most Viewed Artists', 'artist', 5)
+        addDir('Most Liked Artists', 'artist', 6)
+        addDir('Videos by Genre', 'video', 7)
+        addDir('Most Recent Videos', 'http://www.vevo.com/videos?order=MostRecent', 4)
+        addDir('Most Viewed Videos', 'video', 5)
+        addDir('Most Liked Videos', 'video', 6)
+
+def mostViewed(mode,genre = 'none'):
+        if mode == 'artist':
+                name = 'Artists'
+                mode = 2
+                urlname = 'artists'
+        elif mode == 'video':
+                name = 'Video'
+                mode = 4
+                urlname = 'videos'
+        if genre <> 'none':
+                genreparam = '&genre='+genre
+        else:
+                genreparam = ''
+        addDir('Most Viewed '+name+' Today',            'http://www.vevo.com/'+urlname+'?order=MostViewedToday'+genreparam,     mode)
+        addDir('Most Viewed '+name+' This Week',        'http://www.vevo.com/'+urlname+'?order=MostViewedThisWeek'+genreparam,  mode)
+        addDir('Most Viewed '+name+' This Month',       'http://www.vevo.com/'+urlname+'?order=MostViewedThisMonth'+genreparam, mode)
+        addDir('Most Viewed '+name+' All Time',         'http://www.vevo.com/'+urlname+'?order=MostViewedAllTime'+genreparam,   mode)
+        
+def mostLiked(mode,genre = 'none'):
+        if mode == 'artist':
+                name = 'Artists'
+                mode = 2
+                urlname = 'artists'
+        elif mode == 'video':
+                name = 'Video'
+                mode = 4
+                urlname = 'videos'
+        if genre <> 'none':
+                genreparam = '&genre='+genre
+        else:
+                genreparam = ''
+        addDir('Most Liked '+name+' Today',            'http://www.vevo.com/'+urlname+'?order=MostFavoritedToday'+genreparam,     mode)
+        addDir('Most Liked '+name+' This Week',        'http://www.vevo.com/'+urlname+'?order=MostFavoritedThisWeek'+genreparam,  mode)
+        addDir('Most Liked '+name+' This Month',       'http://www.vevo.com/'+urlname+'?order=MostFavoritedThisMonth'+genreparam, mode)
+        addDir('Most Liked '+name+' All Time',         'http://www.vevo.com/'+urlname+'?order=MostFavoritedAllTime'+genreparam,   mode)
+
+def choiceGenres(url, mode):
+        if mode == 8:
+                urlname = 'artists'
+                mode = 11
+                rmode = 2
+        elif mode == 9:
+                urlname = 'videos'
+                mode = 13
+                rmode= 4
+        addDir('Most Liked',url, mode)
+        addDir('Most Viewed',url, mode+1)
+        addDir('Most Recent', 'http://www.vevo.com/'+urlname+'?order=MostRecent&genre='+url, rmode)
+        
+def listGenres(mode):
+        if mode == 'artist':
+                mode = 8
+                urlname = 'artists'
+        elif mode == 'video':
+                mode = 9
+                urlname = 'videos'    
+        addDir('Alternative','alternative',mode)
+        addDir('Blues','blues',mode)
+        addDir("Children's Music",'childrens-music',mode)
+        addDir('Classical','classical',mode)
+        addDir('Comedy/Humor','comedyhumor',mode)
+        addDir('Country','country',mode)
+        addDir('Electronic/Dance','electronicdance',mode)
+        addDir('Christian & Gospel','christian-gospel',mode)
+        addDir('Holiday','holiday',mode)
+        addDir('Jazz','jazz',mode)
+        addDir('Latino','latino',mode)
+        addDir('Rock','rock',mode)
+        addDir('Pop','pop',mode)
+        addDir('R&B/Soul','rbsoul',mode)
+        addDir('Reggae','reggae',mode)
+        addDir('Rap/Hip-Hop','raphip-hop',mode)
+        addDir('Soundtrack','soundtrack',mode)
+        addDir('Spoken Word','spoken-word',mode)
+        addDir('World','world',mode)
+        addDir('Easy Listening','easy-listening',mode)
+        addDir('Other','other',mode)
 
 def listAZ(url):
         addDir('#', url+urllib.quote_plus('#'), 2)
         alphabet=set(string.ascii_uppercase)
         for letter in alphabet:
-                url = url+letter
-                addDir(letter, url, 2)
+                print url
+                newurl = url+str(letter)
+                addDir(letter, newurl, 2)
         return
 
 def listArtist(url):
@@ -42,6 +126,26 @@ def listArtist(url):
                 thumbnail = artist.a.img['src']
                 title = artist.a.img['alt']
                 addDir(title, url, 3, iconimage=thumbnail)
+
+def listVideos(url):
+        data = getURL(url)
+        tree=BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
+        try:
+                pagenext = tree.find(attrs={'class' : 'next'})
+                addDir(pagenext.string+' Page', BASE_URL+pagenext['href'], 4)
+        except:
+                print 'No Next Page'
+        try:
+                pageprev = tree.find(attrs={'class' : 'prev'})
+                addDir(pageprev.string+' Page', BASE_URL+pageprev['href'], 4)
+        except:
+                print 'No Previous Page'
+        videos = tree.findAll(attrs={'class' : 'listThumb'})
+        for video in videos:
+                url = BASE_URL+video.find(attrs={'class' : 'playOverlay'})['href']
+                thumbnail = video.img['src']
+                title = video.img['alt']
+                addLink(title, url, 10, iconimage=thumbnail)
 
 def listArtistVideos(url):
         data = getURL(url)
@@ -185,6 +289,36 @@ elif mode==2:
 elif mode==3:
         listArtistVideos(url)
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==4:
+        listVideos(url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==5:
+        mostViewed(url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==6:
+        mostLiked(url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==7:
+        listGenres(url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==8:
+        choiceGenres(url, mode)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==9:
+        choiceGenres(url, mode)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==11:
+        mostLiked('artist',url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==12:
+        mostViewed('artist',url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==13:
+        mostLiked('video',url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+elif mode==14:
+        mostViewed('video',url)
+        xbmcplugin.endOfDirectory(int(sys.argv[1])) 
 elif mode==10:
         getVideo(url)
 

5592275510a543447240e1d73d711625f43ca97a
bluecopter <bluecopter@9d917ac4-0eb2-cfcc-ffbc-adfc0d90c138>
2011-02-11 14:28:04 +0000

    The Daily Show - fix for site chanegs The Colbert Report - fix for site changes

--- plugin.video.the.colbert.report/default.py
+++ plugin.video.the.colbert.report/default.py
@@ -230,7 +230,8 @@ def PLAYVIDEO(name,url):
 def PLAYFULLEPISODE(name,url):
         data = getURL(url)
         uri=re.compile('<param name="movie" value="http://media.mtvnservices.com/(.+?)"').findall(data)[0]
-        url = 'http://media.mtvnservices.com/player/config.jhtml?uri='+uri+'&group=entertainment&type=network&site=colbertnation.com'
+        url = 'http://shadow.comedycentral.com/feeds/video_player/mrss/?uri='+uri
+        #url = 'http://media.mtvnservices.com/player/config.jhtml?uri='+uri+'&group=entertainment&type=network&site=colbertnation.com'
         data = getURL(url)
         uris=re.compile('<guid isPermaLink="false">(.+?)</guid>').findall(data)
         stacked_url = 'stack://'
--- plugin.video.the.daily.show/default.py
+++ plugin.video.the.daily.show/default.py
@@ -301,7 +301,8 @@ def PLAYVIDEO(name,url):
 def PLAYFULLEPISODE(name,url):
         data = getURL(url)
         uri=re.compile('<param name="movie" value="http://media.mtvnservices.com/(.+?)"').findall(data)[0]
-        url = 'http://media.mtvnservices.com/player/config.jhtml?uri='+uri+'&group=entertainment&type=network&site=thedailyshow.com'
+        #url = 'http://media.mtvnservices.com/player/config.jhtml?uri='+uri+'&group=entertainment&type=network&site=thedailyshow.com'
+        url = 'http://shadow.comedycentral.com/feeds/video_player/mrss/?uri='+uri
         data = getURL(url)
         uris=re.compile('<guid isPermaLink="false">(.+?)</guid>').findall(data)
         stacked_url = 'stack://'
